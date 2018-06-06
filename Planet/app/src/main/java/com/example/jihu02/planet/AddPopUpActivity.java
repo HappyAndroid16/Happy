bbe5e25fed5e990f93a1bf053c699ae3e8f30e6f
yoonyoung <yoon980208@gmail.com>
2018-06-07 02:00:27 +0900

    팝업창 띄우기_오류 가능성 있음(#5-1)

--- Planet/app/src/main/java/com/example/jihu02/planet/AddPopUpActivity.java
+++ Planet/app/src/main/java/com/example/jihu02/planet/AddPopUpActivity.java
@@ -1,24 +1,21 @@
 package com.example.jihu02.planet;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-public class JoinActivity extends Activity {
+public class AddPopUpActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_join);
+        setContentView(R.layout.activity_add_pop_up);
     }
 
-    public void onJoin2Listener(View view) {
-
-    finish();
-
+    public void onPushClicked(View view) {
+        finish();
     }
 }
